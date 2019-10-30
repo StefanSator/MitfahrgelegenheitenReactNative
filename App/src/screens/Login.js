4d76a346c897115da264612bd1247b82abb382c9
Stefan Sator <stefan.sator@web.de>
2019-10-30 15:17:34 +0100

    Stub Login Screen implemented

--- App/src/screens/Login.js
+++ App/src/screens/Login.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 
 class LoginScreen extends React.Component {
+
+  constructor() {
+    super();
+    this.email = 'email';
+    this.password = 'password';
+    this.state = {
+      inputEmail: '',
+      inputPassword: ''
+    };
+  }
+
+  _checkInput() {
+    if (this.state.inputEmail === this.email && this.state.inputPassword === this.password) {
+      Alert.alert('Login Correct.');
+    } else {
+      Alert.alert('Login Incorrect.');
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-
+        <Input
+          inputStyle={styles.input}
+          autoCapitalize = 'none'
+          placeholder='Email'
+          leftIcon={{ type: 'feather', name: 'mail' }}
+          onChangeText={ (text) => this.setState({inputEmail: text, inputPassword: this.state.inputPassword})}
+        />
+        <Input
+          inputStyle={styles.input}
+          secureTextEntry = {true}
+          placeholder='Password'
+          leftIcon={{ type: 'font-awesome', name: 'lock' }}
+          onChangeText={ (text) => this.setState({inputEmail: this.state.inputEmail, inputPassword: text})}
+        />
+        <Button
+          buttonStyle={styles.loginButton}
+          titleStyle={styles.loginTitle}
+          title="Login"
+          onPress={this._checkInput.bind(this)} 
+        />
       </View>
     );
   }
@@ -17,6 +56,19 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#20639B"
+  },
+  input: {
+    paddingLeft: 20,
+    color: 'white'
+  },
+  loginButton: {
+    backgroundColor: "white",
+    marginTop: 20,
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  loginTitle: {
+    color: '#20639B'
   }
 });
 
